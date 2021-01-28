08002fab81bd0e488a357bb53aeb70595eaf1253
Abul Bashar <adulbashar124@gmail.com>
2021-01-28 23:30:54 +0600

    subtotal is working

--- calculat.js
+++ calculat.js
@@ -1,5 +1,6 @@
 document.getElementById('mobile-plus-btn').addEventListener('click', function(){
     PurchaseMobileAmount('mobile-amount', 1, 1219, "mobile-prize");
+    
 })
 
 document.getElementById('mobile-minus-btn').addEventListener('click',function(){
@@ -14,12 +15,30 @@ document.getElementById('casing-minus-btn').addEventListener('click',function(){
     PurchaseMobileAmount('casing-amount', -1, 59, "casing-prize");
 })
 
+
+
 function PurchaseMobileAmount(plusMinusId, num, productAmount, productPriceID) {
+
+    // use event handler in plus, minus, button of both mobile and casing section
+
     var capturePieces = document.getElementById(plusMinusId).value;
     var convertPiecesAmount = parseFloat(capturePieces);
+
     var convertPiecesAmount = convertPiecesAmount + num;
+
     var totalPrice = productAmount * convertPiecesAmount;
     document.getElementById(plusMinusId).value = convertPiecesAmount;
     document.getElementById(productPriceID).innerText = totalPrice;
-}
 
+    // subtotal amount
+    var captureMobilePrice = document.getElementById('mobile-prize').innerText;
+    var parseCaptureMobilePrice = parseFloat(captureMobilePrice);
+    
+    var captureCasingPrice = document.getElementById('casing-prize').innerText;
+    var parseCaptureCasingPrice = parseFloat(captureCasingPrice);
+
+    var subtotalAmount = parseCaptureMobilePrice + parseCaptureCasingPrice;
+    
+    document.getElementById('subtotal-id').innerText = subtotalAmount;
+    
+}
