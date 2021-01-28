721a85e2477588bb66b65d1afbc9ea6f8e9050db
Abul Bashar <adulbashar124@gmail.com>
2021-01-28 21:03:20 +0600

    increases and decreases amount of mobile (arrange with function)

--- calculat.js
+++ calculat.js
@@ -1,14 +1,14 @@
 document.getElementById('plus-btn').addEventListener('click', function(){
-    var capturePieces = document.getElementById('amount-pieces').value;
-    var convertPiecesAmount = parseFloat(capturePieces);
-    var convertPiecesAmount = convertPiecesAmount + 1;
-    document.getElementById('amount-pieces').value = convertPiecesAmount;
+    PurchaseMobileAmount('amount-pieces', 1);
+   
 })
 
 document.getElementById('minus-btn').addEventListener('click',function(){
-    var capturePieces = document.getElementById('amount-pieces').value;
-    var convertPiecesAmount = parseFloat(capturePieces);
-    var convertPiecesAmount = convertPiecesAmount - 1;
-    document.getElementById('amount-pieces').value = convertPiecesAmount;
+    PurchaseMobileAmount('amount-pieces', -1);
 })
-
+function PurchaseMobileAmount(id, num) {
+    var capturePieces = document.getElementById(id).value;
+    var convertPiecesAmount = parseFloat(capturePieces);
+    var convertPiecesAmount = convertPiecesAmount + num;
+    document.getElementById('amount-pieces').value = convertPiecesAmount;
+}
